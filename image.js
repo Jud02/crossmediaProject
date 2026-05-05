7ca3a5901ef0513c2c4613f04bbc5f6b7581e57a
Justin Siebert <19justinsiebert99@gmail.com>
2026-05-05 20:56:58 +0200

    changed structure to own hmtlsites

---
+++ image.js
@@ -0,0 +1,17 @@
+
+const imageInput = document.getElementById('imageInput');
+const previewImage = document.getElementById('preview');
+
+imageInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+            
+    if (file) {
+        const reader = new FileReader();
+                
+        reader.onload = function(e) {
+                    previewImage.src = e.target.result; 
+                    previewImage.style.display = 'block';
+        }        
+        reader.readAsDataURL(file); 
+    }
+});
